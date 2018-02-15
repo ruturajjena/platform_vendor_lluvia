8b6668d096cc207403efc9f33d359b3016514f3f
Paul Keith <javelinanddart@gmail.com>
2018-02-15 21:46:00 +0100

    lineage: soong: Add var for linker shim libs
    * To limit security exposure, we're forcing all devices
      to define shim libs with a TARGET_LD_SHIM_LIBS
    
    Change-Id: Ic8722c42807429f2faa3546316c71c40533ce195

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,5 +1,13 @@
-package android
+package android_lluvia
 type Product_variables struct {
+	Needs_text_relocations struct {
+		Cppflags []string
+	}
+
+	Uses_non_treble_camera struct {
+		Cflags []string
+	}
+
 	Has_legacy_camera_hal1 struct {
 		Cflags []string
 	}
@@ -8,28 +16,56 @@ type Product_variables struct {
 		Cflags []string
 	}
 
-	Needs_text_relocations struct {
+	BoardUsesQTIHardware struct {
+		Cflags []string
 		Cppflags []string
 	}
 
-	Target_shim_libs struct {
+	BoardUsesQCOMHardware struct {
+		Cflags []string
 		Cppflags []string
 	}
+
+	TargetUsesQCOMBsp struct {
+		Cflags []string
+		Cppflags []string
+	}
+
+	TargetUsesQCOMLegacyBsp struct {
+		Cflags []string
+		Cppflags []string
+	}
+
+	BoardUsesLegacyAlsa struct {
+		Cflags []string
+		Cppflags []string
+	}
+
+	Cant_reallocate_omx_buffers struct {
+		Cflags []string
+        }
+
 	Uses_generic_camera_parameter_library struct {
 		Srcs []string
 	}
-	Uses_qti_camera_device struct {
+	
+		Target_shim_libs struct {
 		Cppflags []string
-		Shared_libs []string
 	}
 }
 
 type ProductVariables struct {
+	Needs_text_relocations  *bool `json:",omitempty"`
+	Uses_non_treble_camera  *bool `json:",omitempty"`
+	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
+	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
 	Uses_media_extensions   *bool `json:",omitempty"`
-	Needs_text_relocations  *bool `json:",omitempty"`
-	Specific_camera_parameter_library  *string `json:",omitempty"`
+	BoardUsesQTIHardware  *bool `json:",omitempty"`
+	BoardUsesQCOMHardware  *bool `json:",omitempty"`
+	TargetUsesQCOMBsp  *bool `json:",omitempty"`
+	TargetUsesQCOMLegacyBsp  *bool `json:",omitempty"`
+	BoardUsesLegacyAlsa  *bool `json:",omitempty"`
+	Cant_reallocate_omx_buffers *bool `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
-	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
-	Uses_qti_camera_device  *bool `json:",omitempty"`
 }
