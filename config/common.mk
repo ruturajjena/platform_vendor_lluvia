AICP/vendor_aicp

Add all Pixel sounds
pulled from latest O sailfish img

Change-Id: I402094eb0cca23ceb0d6d55328e9e192a414c46a

config/common.mk
@@ -194,5 +194,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.modversion=$(GZOSP_MOD_VERSION) \
     ro.gzosp.buildtype=$(GZOSP_BUILD_TYPE)
 
-EXTENDED_POST_PROCESS_PROPS := vendor/gzosp/tools/gzosp_process_props.py
+# Google sounds
+include vendor/androidx/google/GoogleAudio.mk
 
+EXTENDED_POST_PROCESS_PROPS := vendor/gzosp/tools/gzosp_process_props.py

ruturajjena/platform_vendor_lluvia

Signed-off-by: leodiram <sriramscience7@gmail.com>

config/common.mk
@@ -238,8 +238,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.modversion=$(lluvia_MOD_VERSION) \
     ro.lluvia.buildtype=$(lluvia_BUILD_TYPE) \
 
-EXTENDED_POST_PROCESS_PROPS := vendor/lluvia/tools/lluvia_process_props.py
-
 # Google sounds
 include vendor/lluvia/google/GoogleAudio.mk
 
+
+EXTENDED_POST_PROCESS_PROPS := vendor/lluvia/tools/gzosp_process_props.py