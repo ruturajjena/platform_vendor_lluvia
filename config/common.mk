manishjvora/CANDY_vendor_candy

Include Pixel's unlimited storage in Google Photos feature
This may (or may not) work with other non-Pixel devices. Definitely worth a shot!

Thanks @matthewdalex for providing this info

Change-Id: I771badec39f2264b25cf5ebb3666430f62f072c7
Signed-off-by: calebcabob <calphonic@gmail.com>

config/common.mk
@@ -237,3 +237,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Google sounds
 include vendor/candy/google/GoogleAudio.mk
+
+# Unlimited photo storage in Google Photos
+PRODUCT_COPY_FILES += \
+    vendor/candy/prebuilt/etc/sysconfig/pixel_2017_exclusive.xml:system/etc/sysconfig/pixel_2017_exclusive.xml

ruturajjena/platform_vendor_lluvia

Signed-off-by: leodiram <sriramscience7@gmail.com>

config/common.mk
@@ -267,5 +267,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Google sounds
 include vendor/lluvia/google/GoogleAudio.mk
 
+# Unlimited photo storage in Google Photos
+PRODUCT_COPY_FILES += \
+    vendor/lluvia/prebuilt/etc/sysconfig/pixel_2017_exclusive.xml:system/etc/sysconfig/pixel_2017_exclusive.xml
 
-EXTENDED_POST_PROCESS_PROPS := vendor/lluvia/tools/gzosp_process_props.py
+EXTENDED_POST_PROCESS_PROPS := vendor/lluvia/tools/lluvia_process_props.py