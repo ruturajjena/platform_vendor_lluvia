d7cbb7d10aacab812d25d9a6864c978577ce2948
sriram prakhya <sriramscience7@gmail.com>
2018-06-27 04:50:53 +0530

    well its My Mistake

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(curl -s https://github.com/LLuviaOS/platform_vendor_lluvia/blob/llovizna-release/lluvia.devices | sed -e 's/#.*$//' | awk '{printf "lluvia_%s-%s\n", $1, $2}')
+for combo in $(curl -s https://raw.githubusercontent.com/LLuviaOS/platform_vendor_lluvia/llovizna-release/lluvia.devices | sed -e 's/#.*$//' | awk '{printf "lluvia_%s-%s\n", $1, $2}')
 do
     add_lunch_combo $combo
 done
